5e78629672a78e93aa2695e774bcf6a81b2032ca
jh.jun <jeonjihoon93@naver.com>
2023-02-16 16:59:31 +0900

    Initial commit

---
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -0,0 +1,16 @@
+package tobyspring.helloboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+
+public class HellobootApplication {
+
+	public static void main(String[] args) {
+		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer();
+		webServer.start();
+	}
+
+}
