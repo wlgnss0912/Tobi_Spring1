acd5b1ecfffb2c6101700cabe202138f766eb4b6
jh.jun <jeonjihoon93@naver.com>
2023-02-22 17:29:45 +0900

    프론트 컨트롤러로 전환

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -2,41 +2,22 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class HellobootApplication {
 
 	public static void main(String[] args) {
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+		applicationContext.registerBean(HelloController.class);//어떤 class를 이용해서 Bean class를 생성할 것인가에 대한 Meta data를 넣어줌
+		applicationContext.registerBean(SimpleHelloService.class);
+		applicationContext.refresh();//구성정보로 container 초기화 -> Bean Object 생성
+
 		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("frontcontroller", new HttpServlet() {
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					//인증, 보안, 다국어, 공통 기능 처리
-					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())){
-						String name = req.getParameter("name");
-
-						resp.setStatus(HttpStatus.OK.value());
-						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-						resp.getWriter().println("Hello " + name);
-					}
-					else if (req.getRequestURI().equals("/user")){
-						//
-					} else {
-						resp.setStatus(HttpStatus.NOT_FOUND.value());
-					}
-
-				}
-			}).addMapping("/*");
+			servletContext.addServlet("dispatcherServlet",
+						new DispatcherServlet(applicationContext)
+					).addMapping("/*");
 		});
 		webServer.start();
 	}
